d65ec9c779cc099c2ba827478c55ba0f798a939a
Samantha Wolff <samanthawolff07@gmail.com>
2022-06-17 22:29:06 -0500

    initial contact form structure

--- photo-port/src/components/Contact/index.js
+++ photo-port/src/components/Contact/index.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 
 function ContactForm() {
-
-    // JSX
-
+    return (
+        <section>
+            <h1>Contact me</h1>
+            <form id="contact-form">
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" name="email" />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <input name="message" rows="5" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+    )
 }
 
 
